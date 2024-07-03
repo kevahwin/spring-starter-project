9f2b61b857372ba91d8f8b1e510078abf284b361
Kevin Ahwin <kevahwin@gmail.com>
2024-07-03 16:46:09 +0100

    Initial commit

---
+++ src/main/java/com/practice/project_enrolment/ProjectEnrolmentApplication.java
@@ -0,0 +1,13 @@
+package com.practice.project_enrolment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEnrolmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEnrolmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/project_enrolment/ProjectEnrolmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.project_enrolment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEnrolmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
